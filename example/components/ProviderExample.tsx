f109d207df21ee7d14538eb6d23f425a67707871
cj <745960380@qq.com>
2023-08-24 23:43:51 +0800

    1

--- example/components/ProviderExample.tsx
+++ example/components/ProviderExample.tsx
@@ -1,35 +1,67 @@
-import type { CoinbaseWallet } from '@web3-react/coinbase-wallet'
-import { useWeb3React, Web3ReactHooks, Web3ReactProvider } from '@web3-react/core'
-import type { MetaMask } from '@web3-react/metamask'
-import type { Network } from '@web3-react/network'
-import type { WalletConnect } from '@web3-react/walletconnect'
-import type { WalletConnect as WalletConnectV2 } from '@web3-react/walletconnect-v2'
+import type { CoinbaseWallet } from "@web3-react/coinbase-wallet";
+import {
+  useWeb3React,
+  Web3ReactHooks,
+  Web3ReactProvider,
+} from "@web3-react/core";
+import type { MetaMask } from "@web3-react/metamask";
+import type { Network } from "@web3-react/network";
+import type { WalletConnect } from "@web3-react/walletconnect";
+import type { WalletConnect as WalletConnectV2 } from "@web3-react/walletconnect-v2";
 
-import { coinbaseWallet, hooks as coinbaseWalletHooks } from '../connectors/coinbaseWallet'
-import { hooks as metaMaskHooks, metaMask } from '../connectors/metaMask'
-import { hooks as networkHooks, network } from '../connectors/network'
-import { hooks as walletConnectHooks, walletConnect } from '../connectors/walletConnect'
-import { hooks as walletConnectV2Hooks, walletConnectV2 } from '../connectors/walletConnectV2'
-import { getName } from '../utils'
+import {
+  coinbaseWallet,
+  hooks as coinbaseWalletHooks,
+} from "../connectors/coinbaseWallet";
+import { hooks as metaMaskHooks, metaMask } from "../connectors/metaMask";
+import { hooks as networkHooks, network } from "../connectors/network";
+import {
+  hooks as walletConnectHooks,
+  walletConnect,
+} from "../connectors/walletConnect";
+import {
+  hooks as walletConnectV2Hooks,
+  walletConnectV2,
+} from "../connectors/walletConnectV2";
+import { getName } from "../utils";
+import { BlockNumberProvider } from "./BlockNumber";
+import { useEffect } from "react";
 
-const connectors: [MetaMask | WalletConnect | WalletConnectV2 | CoinbaseWallet | Network, Web3ReactHooks][] = [
+const connectors: [
+  MetaMask | WalletConnect | WalletConnectV2 | CoinbaseWallet | Network,
+  Web3ReactHooks
+][] = [
   [metaMask, metaMaskHooks],
   [walletConnect, walletConnectHooks],
   [walletConnectV2, walletConnectV2Hooks],
   [coinbaseWallet, coinbaseWalletHooks],
   [network, networkHooks],
-]
+];
 
 function Child() {
-  const { connector } = useWeb3React()
-  console.log(`Priority Connector is: ${getName(connector)}`)
-  return null
+  const { connector, provider } = useWeb3React();
+  console.log(`Priority Connector is: ${getName(connector)}`);
+
+  useEffect(() => {
+    if (!provider) return;
+    const ttt = setInterval(() => {
+      provider.getBlockNumber("latest").then((blockNumber) => {
+        console.log("当前区块高度", blockNumber, "blockNumber多少");
+      });
+    }, 1000);
+    return () => {
+      clearInterval(ttt);
+    };
+  }, [provider]);
+  return null;
 }
 
 export default function ProviderExample() {
   return (
     <Web3ReactProvider connectors={connectors}>
-      <Child />
+      <BlockNumberProvider>
+        <Child />
+      </BlockNumberProvider>
     </Web3ReactProvider>
-  )
+  );
 }
